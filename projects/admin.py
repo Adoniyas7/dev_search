b93d5a6e8ea5c9538c139934a88a93addeb9abec
Adoniyas7 <adoniyasg7s@gmail.com>
2023-06-07 11:11:31 +0300

    created tag and review model

--- projects/admin.py
+++ projects/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Project
+from .models import Project, Tag, Review
 
 # Register your models here.
 
@@ -7,3 +7,5 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ("id", "title")
 
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Tag)
+admin.site.register(Review)
--- projects/models.py
+++ projects/models.py
@@ -13,10 +13,32 @@ class Project(models.Model):
     source_link = models.CharField(max_length=200, null= True, blank=True)
     vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     vote_count = models.IntegerField(default=0, null=True, blank=True)
-    #tags = models
+    tags = models.ManyToManyField('Tag', blank = True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
         return self.title
+    
+class Tag(models.Model):
+    id = models.UUIDField(default=uuid4, unique=True, primary_key=True, editable=False)
+    name = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Review(models.Model):
+    VOTE_TYPE = (
+        ("up", "Up Vote"),
+        ("down", "Down Vote"),
+    )
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(max_length=200, choices=VOTE_TYPE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.value
 
 
