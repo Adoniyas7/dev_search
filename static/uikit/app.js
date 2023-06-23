a95e70c8a71c7430e4704014c998a0658dadab70
Adoniyas7 <adoniyasg7s@gmail.com>
2023-06-23 11:49:54 +0300

    bug fix

--- static/uikit/app.js
+++ static/uikit/app.js
@@ -1,4 +1,11 @@
 // Invoke Functions Call on Document Loaded
-document.addEventListener('DOMContentLoaded', function () {
-  hljs.highlightAll();
-});
+// document.addEventListener('DOMContentLoaded', function () {
+//   hljs.highlightAll();
+// });
+
+let alert = document.querySelector('.alert');
+let closeBtn = document.querySelector('.alert__close');
+
+closeBtn.addEventListener('click' ,function (){
+  alert.remove();
+  } )
