e9511195df0806c36694c462f2d2bb124e94a550
Adoniyas7 <adoniyasg7s@gmail.com>
2023-07-02 21:08:28 +0300

    added profile serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer, Serializer
 from projects.models import Project
+from users.models import Profile
 
 
+class ProjectSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Profile
+        fields = '__all__'
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
 
+class 
