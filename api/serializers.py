474cabf363be3da87edfd32374378963baba31d1
Adoniyas7 <adoniyasg7s@gmail.com>
2023-07-02 21:11:30 +0300

    fixed typo

--- api/serializers.py
+++ api/serializers.py
@@ -4,14 +4,13 @@ from projects.models import Project
 from users.models import Profile
 
 
-class ProjectSerializer(serializers.ModelSerializer):
-    
+class ProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Profile
         fields = '__all__'
 class ProjectSerializer(serializers.ModelSerializer):
+    owner = ProfileSerializer(many=False)
     class Meta:
         model = Project
         fields = '__all__'
-
-class 
